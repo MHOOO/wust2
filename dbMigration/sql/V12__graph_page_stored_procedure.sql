72b487008a82793c70239bc9ae3eeb71afaabcbb
Felix Dietze <felix@beerleader.net>
2017-06-01 17:07:46 +0200

    wip

--- dbMigration/sql/V12__graph_page_stored_procedure.sql
+++ dbMigration/sql/V12__graph_page_stored_procedure.sql
@@ -1,35 +1,104 @@
--- create or replace function graphpage(connectionstart integer[], connectionend integer[]) returns setof integer as $$
--- declare
---     queue Integer[] := array[start];
--- begin
---     create temporary table visited (id integer NOT NULL) on commit drop;
---     create unique index on visited (id);
 
---     WHILE array_length(queue,1) > 0 LOOP
---         insert into visited (select unnest(queue)) on conflict do nothing;
---         queue := array(
---             select b
---             from (select unnest(queue) as id) as q
---             join edges on a = q.id
---             left outer join visited on b = visited.id
---             where visited.id is NULL
---         );
---     END LOOP;
---     return query (select id from visited);
--- end;
--- $$ language plpgsql;
+drop function create_traversal_function(name text, edge regclass, edge_source text, edge_target text);
+create function create_traversal_function(name text, edge regclass, edge_source text, edge_target text) returns void as $$
+begin
+EXECUTE '
+create function ' || quote_ident(name) || '(start integer[]) returns integer[] as $func$
+declare
+    queue Integer[] := start;
+begin
+    WHILE array_length(queue,1) > 0 LOOP
+        insert into visited (select unnest(queue)) on conflict do nothing;
+        queue := array(
+            select '|| quote_ident(edge_target) ||'
+            from (select unnest(queue) as id) as q
+            join connection on '|| quote_ident(edge_source) ||' = q.id
+            left outer join visited on '|| quote_ident(edge_target) ||' = visited.id
+            where visited.id is NULL
+        );
+    END LOOP;
+    return array (select id from visited);
+end;
+$func$ language plpgsql;
+';
+end
+$$ language plpgsql;
+
+DROP FUNCTION traverse_targets(integer[]);
+DROP FUNCTION traverse_sources(integer[]);
+DROP FUNCTION traverse_parents(integer[]);
+DROP FUNCTION traverse_children(integer[]);
+select create_traversal_function('traverse_targets', 'connection', 'sourceid', 'targetid');
+select create_traversal_function('traverse_sources', 'connection', 'targetid', 'sourceid');
+select create_traversal_function('traverse_children', 'containment', 'parentid', 'childid');
+select create_traversal_function('traverse_parents', 'containment', 'childid', 'parentid');
 
 -- induced subgraph: postids -> ajacency list
 DROP FUNCTION induced_subgraph(integer[]);
-create or replace function induced_subgraph(postids integer[]) returns table(postid integer, title text, targetids integer[]) as $$
-begin
-    return query (
-        select post.id, post.title, array_agg(c.targetid)
+create function induced_subgraph(postids integer[]) returns table(postid integer, title text, targetids integer[], childids integer[]) as $$
+    select
+    id, post.title,
+    array_remove(array_agg(post.targetid), NULL),
+    array_remove(array_agg(post.childid), NULL)
+
+    from (
+        select post.id, post.title, conn.targetid, NULL as childid
         from post
-        left join connection c on c.sourceid = post.id AND c.targetid = ANY(postids)
+        left outer join connection conn on conn.sourceid = post.id AND conn.targetid = ANY(postids)
         where post.id = ANY(postids)
-        group by post.id, post.title
-    );
+
+        UNION ALL
+
+        select post.id, post.title, NULL as targetid, cont.childid
+        from post
+        left outer join containment cont on cont.parentid = post.id AND cont.childid = ANY(postids)
+        where post.id = ANY(postids)
+    ) post
+
+    group by (post.id, post.title)
+$$ language sql;
+
+
+drop function graph_page_posts(conn_sources integer[], conn_targets integer[]);
+create function graph_page_posts(conn_sources integer[], conn_targets integer[]) returns integer[] as $$
+declare
+    sources integer[];
+    targets integer[];
+begin
+    -- we have to privde the temporary visited table for the traversal functions,
+    -- since it is not possible to create function local temporary tables.
+    -- Creating is transaction local, which leads to "table 'visited' already exists" errors,
+    -- when created inside functions.
+    create temporary table visited (id integer NOT NULL) on commit drop;
+    create unique index on visited (id);
+
+    -- each traversal gets a prefilled set of visited vertices ( the ones where the traversal should stop )
+
+    -- walk from conn_targets in source direction until hitting conn_sources (includes both conn_targets and conn_sources)
+    insert into visited (select unnest(conn_sources)) on conflict do nothing;
+    sources := traverse_sources(conn_targets);
+
+    -- walk from conn_sources in target direction until hitting conn_targets (includes both conn_sources and conn_targets)
+    truncate table visited; -- truncating is needed, because else traversal stops at the already visited vertices, even if they have further unvisited incoming edges
+    insert into visited (select unnest(conn_targets)) on conflict do nothing;
+    targets := traverse_targets(conn_sources);
+
+    return array( select distinct unnest(sources || targets)); -- return distinct postids
 end;
 $$ language plpgsql;
-select * from induced_subgraph('{11,12,13,14}'::int[]);
+
+drop function graph_page(conn_sources integer[], conn_targets integer[]);
+create function graph_page(conn_sources integer[], conn_targets integer[]) returns table(postid integer, title text, targetids integer[], childids integer[]) as $$
+    select induced_subgraph(graph_page_posts(conn_sources, conn_targets));
+$$ language sql;
+
+
+update post set title = id;
+-- select * from post order by id;
+-- select * from induced_subgraph(array(select id from post));
+
+-- select * from traversetargets(array[412]);
+-- select * from traversesources(array[412]);
+
+-- select * from graph_page_posts(array[412, 417], array[419]);
+select * from graph_page(array[413], array[419]);
