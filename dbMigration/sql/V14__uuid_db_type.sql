7168042904ac0f427d4ab60fde9c3a60e2bf81f3
johannes karoff <johannes@karoff.net>
2017-06-30 17:44:45 +0200

    fix migration

--- dbMigration/sql/V14__uuid_db_type.sql
+++ dbMigration/sql/V14__uuid_db_type.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION base36_to_uuid(IN base36 varchar) RETURNS bigint AS $$
+CREATE OR REPLACE FUNCTION base36_decode(IN base36 varchar) RETURNS bigint AS $$
 DECLARE
     a char[];
     ret bigint;
@@ -29,13 +29,11 @@ DECLARE
     v_base36 CHARACTER VARYING(24) DEFAULT NULL;
 BEGIN
     IF char_length(v_input) <> 25 THEN
-        RETURN v_input;
-    ELSE
-        RAISE NOTICE 'good: %, length: %', v_input, char_length(v_input);
+        RETURN lpad(to_hex(CAST (v_input as INTEGER)), 32, '0');
     END IF;
 
     v_base36 := right(v_input, -1)::varchar(24);
-    v_uuid := lpad(to_hex(base36_to_uuid(right(v_base36, -12))), 16, '0') || lpad(to_hex(base36_to_uuid(left(v_base36, -12))), 16, '0');
+    v_uuid := lpad(to_hex(base36_decode(right(v_base36, -12))), 16, '0') || lpad(to_hex(base36_decode(left(v_base36, -12))), 16, '0');
     RETURN v_uuid;
 END;
 $$ language plpgsql;
@@ -55,16 +53,22 @@ ALTER TABLE ownership
 
 DROP VIEW post;
 
+UPDATE rawpost SET id = base36_to_base16(id);
 ALTER TABLE rawpost
-    ALTER COLUMN id TYPE UUID;
+    ALTER COLUMN id TYPE UUID USING id::UUID;
+UPDATE connection SET sourceId = base36_to_base16(sourceId);
+UPDATE connection SET targetId = base36_to_base16(targetId);
 ALTER TABLE connection
-    ALTER COLUMN sourceId TYPE UUID,
-    ALTER COLUMN targetId TYPE UUID;
+    ALTER COLUMN sourceId TYPE UUID USING sourceId::UUID,
+    ALTER COLUMN targetId TYPE UUID USING targetId::UUID;
+UPDATE containment SET parentId = base36_to_base16(parentId);
+UPDATE containment SET childId = base36_to_base16(childId);
 ALTER TABLE containment
-    ALTER COLUMN parentId TYPE UUID,
-    ALTER COLUMN childId TYPE UUID;
+    ALTER COLUMN parentId TYPE UUID USING parentId::UUID,
+    ALTER COLUMN childId TYPE UUID USING childId::UUID;
+UPDATE ownership SET postId = base36_to_base16(postId);
 ALTER TABLE ownership
-    ALTER COLUMN postId TYPE UUID;
+    ALTER COLUMN postId TYPE UUID USING postId::UUID;
 
 ALTER TABLE connection
     ADD constraint connection_sourceid_fkey foreign key (sourceId) references rawpost (id) on delete cascade,
@@ -82,4 +86,4 @@ ALTER TABLE ownership
 CREATE VIEW post AS SELECT id,title FROM rawpost WHERE isdeleted = false;
 
 DROP FUNCTION base36_decode(varchar);
-DROP FUNCTION convert_to_uuid(CHARACTER VARYING(36));
+DROP FUNCTION base36_to_base16(CHARACTER VARYING(36));
